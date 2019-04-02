13061d500b18064f9a0f1c0fccbc601a1317fae2
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro.local>
2019-04-02 15:55:16 -0600

    prob1 done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    text: ''
+  }
+  updateText=(event)=>{
+    console.log(event.target.value)
+    this.setState({
+      text: event.target.value
+    })
+
+  }
   render() {
     return (
       <div className="App">
-       checking
+       <input onChange={this.updateText}></input>
+       <h1>{this.state.text}</h1>
       </div>
-    );
+    
+      );
   }
 }
 
